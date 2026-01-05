cb0072537e187e8e6a47666907cc2f57278adf64
Mayuri Fulpagare <mayurifulpagare13@gmail.com>
2026-01-06 01:53:19 +0530

    feat: Enhance HomeScreen with dialog and bottom sheet examples

--- Getx_tutorial/getx_tutorial_1/lib/home_screen.dart
+++ Getx_tutorial/getx_tutorial_1/lib/home_screen.dart
@@ -1,20 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
-import 'package:getx_tutorial_1/main.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Getx Tutorials'),
-      ),
+      appBar: AppBar(title: Text('Getx Tutorials')),
       body: Column(
         children: [
-          Text('Home Screen'),
+          Card(
+            child: ListTile(
+              title: Text('GetX Dialog Alert Example'),
+              subtitle: Text('GetX dialog alert with GetX'),
+              onTap: () {
+                Get.defaultDialog(
+                  title: 'GetX Dialog',
+                  middleText: 'This is a GetX dialog alert example',
+                  titlePadding: EdgeInsets.all(20),
+                  contentPadding: EdgeInsets.all(20),
+                  textCancel: 'Cancel',
+                  textConfirm: 'Confirm',
+                  onCancel: () {
+                    Get.back();
+                    print('Dialog Cancelled');
+                  },
+                  onConfirm: () {
+                    Get.back();
+                    print('Dialog Confirmed');
+                  },
+                );
+              },
+            ),
+          ),
+
+          Card(
+            child: ListTile(
+              title: Text('GetX bottomsheet Example'),
+              subtitle: Text('GetX bottomsheet with GetX'),
+              onTap: () {
+                Get.bottomSheet(
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(30),
+                      ),
+                    ),
+                    height: 400,
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: Icon(Icons.light_mode),
+                          title: Text('Theme Light Mode'),
+                          onTap: () {
+                            Get.changeTheme(ThemeData.light());
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.dark_mode),
+                          title: Text('Theme Dark Mode'),
+                          onTap: () {
+                            Get.changeTheme(ThemeData.dark());
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -26,7 +84,7 @@ class HomeScreen extends StatelessWidget {
             icon: Icon(Icons.notification_add, color: Colors.white),
             backgroundColor: const Color.fromARGB(255, 192, 176, 218),
             onTap: (snack) {
-              TextButton( 
+              TextButton(
                 onPressed: () {
                   print('Snackbar button pressed');
                 },
