44b21181642605f28a5ec12a418af2ee006d5f90
Mayuri Fulpagare <mayurifulpagare13@gmail.com>
2026-01-12 00:03:49 +0530

    feat: Add Flutter project list and learning roadmap PDFs

--- first.dart
+++ first.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 void main() {
   print('Hello, Dart!');
+
+  stdout.write("Enter your Name:");
+
+  var Name = stdin.readLineSync();
+
+  print("Welcome $Name to Dart Programming");
 }
-
-
